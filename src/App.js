c88cc93e85a21431ff09fb62ed1b97eca11150b0
reveza <rebeccavezinacote@gmail.com>
2018-01-04 20:27:19 -0500

    Remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import './index.css';
 
 import Header from './Header';
@@ -7,14 +7,12 @@ class App extends Component {
   render() {
     return(
       <div className="App p-3">
-      <div className="row">
-        <div className="col-md-2 col-xs-0">
-        </div>
-        <div className="col-md-8 col-xs-12">
-          <Header />
+        <div className="row">
+          <div className="col-md-2 col-xs-0"></div>
+          <div className="col-md-8 col-xs-12">
+            <Header />
           </div>
-        <div className="col-md-2 col-xs-0">
-        </div>
+          <div className="col-md-2 col-xs-0"></div>
         </div>
         <a rel="stylesheet" type="text/css" href="" className="link text-center">Unsubscribe</a>
       </div>
